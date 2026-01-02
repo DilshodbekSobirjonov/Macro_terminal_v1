1a17562ccee1fad0607bfc5313732458dfcb1eb0
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2026-01-02 16:45:19 +0900

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -1,36 +1,19 @@
-from services.exchange import ExchangeService
-from core.candles import CandleFrame
-from core.scoring import calculate_score
+from services.telegram import TelegramService
 from trades.trade import Trade
-from trades.monitor import monitor_trade
 
-exchange = ExchangeService()
+tg = TelegramService(
+    token="8543769576:AAGmG7nQjuezf5OqHqzAWlZI1GJsP3b_Gyw",
+    chat_id="-1003534899420"
+)
 
-symbol = "BTCUSDT"
-tf = "30m"
-
-ohlcv = exchange.fetch_ohlcv(symbol, tf, limit=200)
-oi_series = exchange.fetch_open_interest_history(symbol, tf, limit=200)
-
-candles = CandleFrame(ohlcv, oi_series)
-
-score, reasons = calculate_score(candles.candles)
-
-print("SCORE:", score)
-print("REASONS:", reasons)
-
-# ❗ ИСКУССТВЕННАЯ СДЕЛКА ДЛЯ ТЕСТА
 trade = Trade(
-    pair=symbol,
+    pair="BTCUSDT",
     direction="LONG",
-    entry_price=candles.candles[-1].close,
-    score=score,
-    reasons=reasons,
+    entry_price=88899,
+    score=5,
+    reasons=["CHoCH", "Volume spike", "OI increasing"],
     tp_levels=[3, 6, 10],
     sl_percent=4.5
 )
 
-event = monitor_trade(trade, candles.candles)
-print("TRADE EVENT:", event)
-print("CURRENT PNL:", trade.current_profit)
-print("STATE:", trade.state)
+tg.send("🔥 TEST MESSAGE")
