dacc954d9c233b53b6d869f075f50c9974f5484c
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2026-01-02 22:09:22 +0900

    Update logger.py

--- services/logger.py
+++ services/logger.py
@@ -3,19 +3,32 @@
 import logging
 from logging.handlers import RotatingFileHandler
 
+
 def setup_logger():
     logger = logging.getLogger("macroterminal")
     logger.setLevel(logging.INFO)
 
-    handler = RotatingFileHandler(
+    # ⚠️ чтобы не дублировались логи при рестарте
+    if logger.handlers:
+        return logger
+
+    # ===== File logger (rotation) =====
+    file_handler = RotatingFileHandler(
         "macroterminal.log",
         maxBytes=2 * 1024 * 1024,  # 2 MB
         backupCount=3
     )
+
     formatter = logging.Formatter(
         "%(asctime)s | %(levelname)s | %(message)s"
     )
-    handler.setFormatter(formatter)
 
-    logger.addHandler(handler)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # ===== Console logger (Termux) =====
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
     return logger
