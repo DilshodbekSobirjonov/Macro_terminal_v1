e85146fd39096c8a60aff445e778fd5dbd8dffa1
DilshodbekSobirjonov <77490108+DilshodbekSobirjonov@users.noreply.github.com>
2026-01-02 17:07:17 +0900

    Create config/symbols.py

---
+++ config/symbols.py
@@ -0,0 +1,14 @@
+# config/symbols.py
+
+SYMBOLS = [
+    "BTCUSDT",
+    "ETHUSDT",
+    "SOLUSDT",
+    "AVAXUSDT",
+    "LINKUSDT",
+    "OPUSDT",
+    "ARBUSDT",
+    "INJUSDT",
+    "APTUSDT",
+    "ATOMUSDT",
+]
